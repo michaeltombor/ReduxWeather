4511a9250729df35b5090287ed7690868863340a
Michael Tombor <tombormt@gmail.com>
2018-03-29 18:56:34 +0000

    add chart component and organized weather data in container

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+  
     return(
       <tr key={name}>
         <td>{name}</td>
+       <td>
+          <Chart data={temps} color="orange" />
+        </td>
+        <td>
+          <Chart data={humidities} color="red" />
+        </td>
+        <td>
+          <Chart data={pressures} color="blue" />
+        </td>
       </tr>
       );
   }
