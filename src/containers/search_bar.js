271837a75029c9a892c844c2f68eb2887e06a72a
Michael Tombor <tombormt@gmail.com>
2018-03-29 17:24:06 +0000

    update reducer and action creater

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component{
+class SearchBar extends Component{
     //To set up our state, we need to initialize it inside of a constructor
     constructor(props){
         super(props);
@@ -8,17 +11,24 @@ export default class SearchBar extends Component{
         this.state={ term: ''}
         //Binding 'this' preserves the context and allows us to use 'this' in our function
         this.onInputChange = this.onInputChange.bind(this)
+        this.onFormSubmit = this.onFormSubmit.bind(this)
     }
     
     onInputChange(event) {
-        console.log(event.target.value)
-        this.setState({term: event.target.value})
+        this.setState({ term: event.target.value })
     }
     
     onFormSubmit(event){
         //tells the browser to not submit the form
+        //A form element gives us the functionality of the input submitting on enter/button click
         event.preventDefault();
+        
+        //We need to go and fetch weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({ term: '' })
     }
+    
+    
     
     render(){
         return(
@@ -36,3 +46,9 @@ export default class SearchBar extends Component{
             );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar)
